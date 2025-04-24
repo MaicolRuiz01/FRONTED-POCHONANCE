2ea1f9b540f32093be9fd161912b83f1d297fbc7
elvin <elvindanielmolo@ufps.edu.co>
2025-04-24 18:53:20 -0500

    crear cuenta binance y listarla, implementado

---
+++ src/app/core/services/account-binance.service.ts
@@ -0,0 +1,24 @@
+// src/app/core/services/account-binance.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environment/environment';
+import { AccountBinance } from '../../modules/account-binance';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountBinanceService {
+  private apiUrl = `${environment.apiUrl}/cuenta-binance`;
+
+  constructor(private http: HttpClient) {}
+
+  traerCuentas(): Observable<AccountBinance[]> {
+    return this.http.get<AccountBinance[]>(this.apiUrl); // Nota el array []
+  }
+
+  crear(account: AccountBinance): Observable<AccountBinance> {
+    return this.http.post<AccountBinance>(this.apiUrl, account);
+  }
+}

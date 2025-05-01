718c07a788699fff8b066f6d7735764717642f6a
elvin <elvindanielmolo@ufps.edu.co>
2025-05-01 17:12:17 -0500

    dialogo de p2p modificado solo de forma visual, se empezó con la creación del modulo de actividades

---
+++ src/app/pages/activadades/criptos/criptos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criptos',
+  standalone: true,
+  imports: [],
+  templateUrl: './criptos.component.html',
+  styleUrl: './criptos.component.css'
+})
+export class CriptosComponent {
+
+}

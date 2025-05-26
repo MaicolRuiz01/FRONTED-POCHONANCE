30976070ac98e876dd5f123b883222cdf11fad2d
Maicol Ruiz <maicolandreyruga@ufps.edu.co>
2025-05-25 21:58:15 -0500

    se corrigieron endpoints y otras cosas y una nueva vista llamada trust

---
+++ src/app/pages/asignaciones/tabs/proveedor-trx-tab/proveedor-trx-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedor-trx-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './proveedor-trx-tab.component.html',
+  styleUrl: './proveedor-trx-tab.component.css'
+})
+export class ProveedorTRXTabComponent {
+
+}
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
     production: false,
 
 
-    /* apiUrl: 'https://backend-binance-resumen-production-afcf.up.railway.app'  // URL base del backend
+    /* apiUrl: 'https://backend-binance-resumen-production-afcf.up.railway.app'  // URL base del backend */
 
-    apiUrl: 'http://localhost:8080' */
+    apiUrl: 'http://localhost:8080'
 
-    apiUrl: 'http://18.231.14.98' //aws
+    /* apiUrl: 'http://18.231.14.98' //aws */
 
   };

1a715b2ea694ab1547a5aab33a21e8fb32a69f25
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2025-07-20 16:15:55 -0500

    actualizacion formilarui y etiqeuta de efectivo

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
 
 
- // apiUrl: 'http://localhost:8080'
+  //apiUrl: 'http://localhost:8080'
 
   apiUrl: 'https://backend-binance-resumen-production.up.railway.app'
 

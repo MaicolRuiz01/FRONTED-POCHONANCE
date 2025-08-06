f76c7464daaf8ac4f17b6d39b4c60c15ebcd2965
Marlon Diaz <marlonjosedito@ufps.edu.co>
2025-08-05 22:19:40 -0500

    creacion seccion cajas en saldos

--- src/app/pages/saldos/container/saldos-tab.component.ts
+++ src/app/pages/saldos/container/saldos-tab.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CajaComponent } from '../tabs/balance/caja.component';
+import { CajasComponent } from '../tabs/cajas/cajas.component';
 import { GastosComponent } from '../tabs/gastos/gastos.component';
 import { SharedModule } from '../../../shared/shared.module';
 import { SaldosComponent } from '../tabs/saldos-cuentas/saldos.component';
@@ -7,7 +8,7 @@ import { SaldosComponent } from '../tabs/saldos-cuentas/saldos.component';
 @Component({
   selector: 'app-saldos-tab',
   standalone: true,
-  imports: [SharedModule, SaldosComponent, CajaComponent, GastosComponent],
+  imports: [SharedModule, SaldosComponent, CajasComponent, CajaComponent, GastosComponent],
   templateUrl: './saldos-tab.component.html',
   styleUrl: './saldos-tab.component.css'
 })

b522ee3484b6d92a9c8ee1f4d0cea3c9494688a5
Marlon Diaz <marlonjosedito@ufps.edu.co>
2025-08-07 21:26:01 -0500

    cambio de lugar para gastos y cuentas

--- src/app/pages/asignaciones/container/asignaciones-wrapper/asignaciones-wrapper.component.ts
+++ src/app/pages/asignaciones/container/asignaciones-wrapper/asignaciones-wrapper.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AsignacionesTabComponent } from '../../tabs/asignaciones-tab/asignaciones-tab.component';
-import { CuentasTabComponent } from '../../tabs/cuentas-tab/cuentas-tab.component';
+import { CuentasTabComponent } from '../../../saldos/tabs/cuentas-tab/cuentas-tab.component';
 import { CommonModule } from '@angular/common';
 import { TabViewModule } from 'primeng/tabview';
 import { TraspasosTabComponent } from '../../tabs/traspasos-tab/traspasos-tab.component';
 import { MovimientosComponent } from "../../tabs/cajas-tab/cajas-tab.component";
+import { GastosComponent } from '../../gastos-tab/gastos-tab.component';
 
 @Component({
   selector: 'app-asignaciones-wrapper',
@@ -15,10 +16,11 @@ import { MovimientosComponent } from "../../tabs/cajas-tab/cajas-tab.component";
     CommonModule,
     TabViewModule,
     TraspasosTabComponent,
-    MovimientosComponent
+    MovimientosComponent,
+    GastosComponent
 ],
   templateUrl: './asignaciones-wrapper.component.html',
-  styleUrl: './asignaciones-wrapper.component.css'
+  styleUrls: ['./asignaciones-wrapper.component.css']
 })
 export class AsignacionesWrapperComponent {
   tabIndex = 0;
--- src/app/pages/saldos/container/saldos-tab.component.ts
+++ src/app/pages/saldos/container/saldos-tab.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CajaComponent } from '../tabs/balance/caja.component';
 import { CajasComponent } from '../tabs/cajas/cajas.component';
-import { GastosComponent } from '../tabs/gastos/gastos.component';
 import { SharedModule } from '../../../shared/shared.module';
 import { SaldosComponent } from '../tabs/saldos-cuentas/saldos.component';
+import { CuentasTabComponent } from '../tabs/cuentas-tab/cuentas-tab.component';
 
 @Component({
   selector: 'app-saldos-tab',
   standalone: true,
-  imports: [SharedModule, SaldosComponent, CajasComponent, CajaComponent, GastosComponent],
+  imports: [SharedModule, SaldosComponent, CajasComponent, CajaComponent, CuentasTabComponent],
   templateUrl: './saldos-tab.component.html',
-  styleUrl: './saldos-tab.component.css'
+  styleUrls: ['./saldos-tab.component.css']
 })
 export class SaldosTabComponent {
 

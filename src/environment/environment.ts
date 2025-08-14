5e2013376e5f088a7eedaaa2065b2ef416ac02e9
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2025-08-14 01:11:55 -0500

    solucion a vacios

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
- apiUrl: 'https://backend-binance-resumen-production.up.railway.app'
+apiUrl: 'https://backend-binance-resumen-production.up.railway.app'
 // apiUrl: 'http://localhost:8080'
 
 

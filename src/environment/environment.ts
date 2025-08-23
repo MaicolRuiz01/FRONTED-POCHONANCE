97db7c95e18b7f77312423b37c293e6cdc0da1cb
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2025-08-23 14:41:27 -0500

    cambios en compras y ventas

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-apiUrl: 'https://backend-binance-resumen-production.up.railway.app'
-// apiUrl: 'http://localhost:8080'
+//apiUrl: 'https://backend-binance-resumen-production.up.railway.app'
+apiUrl: 'http://localhost:8080'
 
 
   };

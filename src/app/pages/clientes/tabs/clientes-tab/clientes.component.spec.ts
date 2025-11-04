e4e6a42ca19a0ea0212a0bf3d3a0ea1b02efcbfd
Marlon Diaz <marlonjosedito@ufps.edu.co>
2025-11-04 16:44:38 -0500

    añadiendo boton eliminar a clientes y proovedores

--- src/app/pages/clientes/tabs/clientes-tab/clientes.component.spec.ts
+++ src/app/pages/clientes/tabs/clientes-tab/clientes.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientesComponent } from './clientes.component';
 
+
 describe('ClientesComponent', () => {
   let component: ClientesComponent;
   let fixture: ComponentFixture<ClientesComponent>;
